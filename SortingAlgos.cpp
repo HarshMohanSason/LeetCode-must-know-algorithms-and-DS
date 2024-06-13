6a2a8be24cc87dfc8b99c9b1773f6aae51061c11
Harsh Mohan Sason <80195000+HarshMohanSason@users.noreply.github.com>
2024-06-13 13:08:07 -0700

    Update SortingAlgos.cpp

--- SortingAlgos.cpp
+++ SortingAlgos.cpp
@@ -53,9 +53,20 @@ void selectionSort(int arr[], int n)
     }
 }
 
-void insertionSort(int arr[], int n)
-{
+void insertionSort(int arr[], int n) {
     
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 void printArray(int arr[], int size) {
